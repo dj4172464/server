b5c1e4441d29227a39f3e372c3008838e07b27a2
lfxgroove <lfxgroove@zoho.com>
2013-08-01 18:46:19 +0200

    [m][2428] Update to ticket handling and hopefully a fix to logging

---
+++ sql/updates/r2428_01_mangos_mangos_string.sql
@@ -0,0 +1,4 @@
+ALTER TABLE db_version CHANGE COLUMN required_r2425_02_mangos_mangos_string required_r2428_01_mangos_mangos_string BIT;
+
+INSERT INTO `mangos_string` VALUES
+(1513,'You cant close a ticket for offline players',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_r2427_02_characters_character_whispers"
- #define REVISION_DB_MANGOS "required_r2425_02_mangos_mangos_string"
+ #define REVISION_DB_MANGOS "required_r2428_02_mangos_mangos_command"
  #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
 #endif // __REVISION_SQL_H__
